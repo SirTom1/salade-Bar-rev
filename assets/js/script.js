762c6c2166faafbb50f8587a18e60f5d59768b8d
SirTom1 <tbourti@edenschool.fr>
2022-06-22 15:11:31 +0200

    my salada bar for revision Edesnchool

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let ArtSelect = document.getElementById('nbrArtSelect');
+let base = 0;
+
+let ingredients = document.getElementById('nbrIngre');
+let TotalPrix = document.getElementById('TotalPrix');
+
+ArtSelect.innerText = base + " / 6";
+ingredients.innerText = base;
+TotalPrix.innerText = base;
